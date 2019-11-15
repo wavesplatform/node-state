bdc80be625884d1572812b1b6bcc19a789f84c17
dtsigelnitskiy <dtsigelnitskiy@wv00505.local>
2019-11-15 15:16:43 +0300

    Fix typescript config

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { outputFile, outputJSON, readFile } from 'fs-extra';
-import { alias, broadcast } from '@waves/waves-transactions';
+import { alias, broadcast, libs } from '@waves/waves-transactions';
 import { CHAIN_ID, MASTER_ACCOUNT_SEED, NODE_URL } from './constants';
 import createAssets from './craeteAssets';
 import createAccounts from './createAccounts';
@@ -24,13 +24,19 @@ export async function write(options: IOptions) {
 }
 
 const exportState = (state: any) => `export const STATE = ${JSON.stringify(state, null, 4)};`;
-const exportConstant = (name: string, value: string | number) => `export const ${name} = "${value}";`;
+const exportConstant = (name: string, value: string | number | object) => `export const ${name} = ${JSON.stringify(value, null, 4)};`;
 const tsTemplate = (state: any) => [
-    exportState(state),
+    exportConstant('MASTER_ACCOUNT', {
+        SEED: MASTER_ACCOUNT_SEED,
+        ADDRESS: libs.crypto.address(MASTER_ACCOUNT_SEED, CHAIN_ID),
+        PUBLIC_KEY: libs.crypto.publicKey(MASTER_ACCOUNT_SEED),
+        ALIAS: 'master'
+    }),
     exportConstant('NODE_URL', NODE_URL),
-    exportConstant('MASTER_ACCOUNT_SEED', MASTER_ACCOUNT_SEED),
     exportConstant('CHAIN_ID', CHAIN_ID),
     exportConstant('NETWORK_BYTE', CHAIN_ID.charCodeAt(0)),
+    '',
+    exportState(state),
 ].join('\n');
 
 export interface IOptions {

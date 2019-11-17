bb4629981057197052c18d14e8f2200711e042c9
Jlewka <ua.lewka@gmail.com>
2019-11-17 17:46:22 +0300

    fix

--- src/craeteAssets.ts
+++ src/craeteAssets.ts
@@ -6,23 +6,23 @@ import console from './console';
 
 
 export default function <ASSETS extends Record<string, IAsset>>(assets: ASSETS): TAssetsResponse<ASSETS> {
-    return Promise.all(Object.entries(assets).map(async ([key, asset]) => {
-        console.log(`Create asset ${key}`);
+  return Promise.all(Object.entries(assets).map(async ([key, asset]) => {
+      console.log(`Create asset ${key}`);
 
-        const tx = issue({
-            chainId: CHAIN_ID,
-            script: typeof asset.script === 'boolean' ? SMART_ASSET_SCRIPT : asset.script,
-            name: asset.name,
-            description: asset.description || `${asset.name} description`,
-            reissuable: asset.reissuable || false,
-            quantity: asset.quantity || 1000000 * Math.pow(10, 8),
-            decimals: asset.decimals || 8,
+      const tx = issue({
+          chainId: CHAIN_ID,
+          script: typeof asset.script === 'boolean' ? SMART_ASSET_SCRIPT : asset.script,
+          name: asset.name,
+          description: asset.description || `${asset.name} description`,
+          reissuable: asset.reissuable || false,
+          quantity: asset.quantity || 1000000 * Math.pow(10, 8),
+          decimals: typeof asset.decimals === 'number' && asset.decimals >= 0 ? asset.decimals : 8
         }, MASTER_ACCOUNT_SEED);
 
-        await broadcastAndWait(tx);
+      await broadcastAndWait(tx);
 
-        return { [key]: tx };
+      return { [key]: tx };
     }))
-        .then(list =>
-            list.reduce((acc, item) => Object.assign(acc, item), Object.create(null))) as any;
+  .then(list =>
+    list.reduce((acc, item) => Object.assign(acc, item), Object.create(null))) as any;
 }
